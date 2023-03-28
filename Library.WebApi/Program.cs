ce6f2e857a02249ebc9bd76899d252ed967cce7a
Арсений Титов <titov.arsenij@yandex.ru>
2023-03-28 12:32:40 +0300

    Delete Startup.cs

--- Library.WebApi/Program.cs
+++ Library.WebApi/Program.cs
@@ -1,12 +1,42 @@
+using Library.Application.Interfaces;
+using Library.Application;
 using Library.Persistance;
 using Library.WebApi;
 using Library.WebApi.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        
+        builder.Services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new AssemblyMappingProfile(typeof(IBooksDbContext).Assembly));
+            config.AddProfile(new AssemblyMappingProfile(typeof(IAuthorDbContext).Assembly));
+        });
+        builder.Services.AddAplication();
+        builder.Services.AddPersistence(builder.Configuration);
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
+        });
+
+        
         var app = builder.Build();
 
         using (var scope = app.Services.CreateScope())
@@ -19,16 +49,31 @@ internal class Program
             }
             catch (Exception exception)
             {
-               
+
             }
-        }      
+        }
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(config =>
+        {
+            config.RoutePrefix = string.Empty;
+            config.SwaggerEndpoint("swagger/v1/swagger.json", "Lib API");
+        });
+        app.UseCustomExceptionHandler();
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseCors("AllowAll");
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
         app.Run();
     }
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-          Host.CreateDefaultBuilder(args)
-              .ConfigureWebHostDefaults(webBuilder =>
-              {
-                  webBuilder.UseStartup<Startup>();
-              });
-    
 }
+    
